b93c023c8b13540cd12203b794781a9e600280a1
aliakbar1881 <aliakbar138118@gmail.com>
2022-11-20 00:32:02 +0000

    Train Model in JupyterNotebook file

---
+++ README.md
@@ -0,0 +1,9 @@
+# Face Mask Detection
+---
+
+Detect images Mask in three status:
+- With Mask
+- Without Mask
+- Mask Not Wear
+
+> dataset : https://www.kaggle.com/datasets/andrewmvd/face-mask-detection
---
+++ init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: init.sh
+# 
+#         USAGE: ./init.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 11/17/2022 18:08
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+export PYTHONPATH=${PWD}
+
+echo 'Done'
